262c215a1c8f7aadcb5f70c81c5f379ed87683e6
Alejandro Sabogal <asabogal.git@gmail.com>
2018-08-19 21:57:14 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -6,8 +6,8 @@ class Helpers
     @user
   end
 
-  def is_logged_in?(session)
-    @user = User.find_by(session[:user_id])
+  def self.is_logged_in?(session)
+    @user = User.find_by_id(session[:user_id])
     @user.id == session[:id] ? true : false
   end
 end
